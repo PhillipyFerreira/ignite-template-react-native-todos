fd2ca3aee8317c37ca04fb6490da84c9631f9864
Phillipy Ferreira <phillipyferreira@MacBook-Pro-de-Phillipy.local>
2022-10-03 19:31:34 -0300

    Testes validados

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,58 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert, StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [createdCount, setCreatedCount] = useState<number>(0);
+
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    if ((tasks.findIndex(task => task.id === 100)) !== -1) {
+      return Alert.alert("Participante existe", "Já existe um participante na lista com esse nome.");
+    }
+    setTasks(prevState => [
+      ...prevState, 
+      {
+        id: Date.now() * Math.random(),
+        title: newTaskTitle,
+        done: false,
+      },
+    ]);
+    setCreatedCount(tasks.length+1)
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const index = tasks.findIndex(task => task.id === id)
+    tasks[index].done = !tasks[index].done
+    const tempTask = tasks[index]
+    setTasks(prevState => prevState.filter(item => item.id !== id))
+    setTasks(prevState => [
+      ...prevState, 
+      tempTask
+    ]);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(prevState => prevState.filter(item => item.id !== id))
+    setCreatedCount(tasks.length-1)
   }
+  
+  useEffect(() => {
+  }, [tasks])
 
   return (
     <View style={styles.container}>
-      <Header tasksCounter={tasks.length} />
+      <Header tasksCounter={createdCount} />
 
       <TodoInput addTask={handleAddTask} />
 
       <TasksList 
-        tasks={tasks} 
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
         removeTask={handleRemoveTask} 
       />
